29ae6bab55ac3d3aa697d11b8b409446ef445a5a
cauvupham11 <caupham25@gmail.com>
2025-05-23 10:12:13 +0700

    api post

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,17 @@
 </template>
 
 <script setup>
-// Không cần import router-view nếu bạn đã .use(router) trong main.js
-// Chỉ import Layout thôi:
+// Import Layout nếu bạn muốn dùng layout bao quanh các trang
 import Layout from '@/components/Layout.vue'
 </script>
 
 <style>
-/* nế﻿u có style chung cho App thì để ở đây */
-</style>
-=======
-  <div id="app">
-    <!-- <Header v-if="!isAdminPage" /> -->
-    <router-view />
-    <!-- <Footer v-if="!isAdminPage" /> -->
-  </div>
-</template>
+/* Nếu có style chung cho App thì để ở đây */
 
-<script>
-// import Header from "./layout/ComHeader.vue";
-
-
-export default {
-  name: "App",
-  components: {
-   // header,
-    
-  },
-  // computed: {
-  //   isAdminPage() {
-  //     return this.$route.path.startsWith("/admin");
-  //   },
-  // },
-};
-</script>
-
-<style>
 #vite-devtools-toggle {
   display: none !important;
 }
+
 html, body, #app {
   margin: 0;
   padding: 0;
@@ -57,7 +30,4 @@ html, body, #app {
   padding: 0;
   box-sizing: inherit;
 }
-
 </style>
-
-
